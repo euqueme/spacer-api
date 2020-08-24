0eec848d68282bc3ee538f1a2fcaeb92d57348f6
Santiago Rodriguez <santo1996.29@gmail.com>
2020-08-24 19:09:36 -0400

    feature: update batch of flashcards

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -22,7 +22,35 @@ class FlashcardsController < ApplicationController
     end
 
     if error_messages.any?
-      render json: { errors: error_messages }, status: :ok
+      render json: { errors: error_messages.unique }, status: :accepted
+    else
+      head :ok
+    end
+  end
+
+  # edits a bunch of flashcards
+  def update
+    # this should be current user
+    user = User.first
+    error_messages = []
+
+    flashcards_bundle_update_params[:_json].each do |flashcard|
+      f = Flashcard.find(flashcard[:id])
+
+      f_temp = user.flashcards.build(
+        front: flashcard[:front],
+        back: flashcard[:back]
+      )
+
+      if f_temp.valid?
+        f.update(front: flashcard[:front], back: flashcard[:back])
+      else
+        error_messages << f_temp.errors.full_messages
+      end
+    end
+
+    if error_messages.any?
+      render json: { errors: error_messages.unique }, status: :accepted
     else
       head :ok
     end
@@ -32,6 +60,10 @@ class FlashcardsController < ApplicationController
 
   def flashcards_bundle_params
     params.permit(_json: [:front, :back])
+  end
+
+  def flashcards_bundle_update_params
+    params.permit(_json: [:id, :front, :back])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  patch 'flashcards', to: 'flashcards#update', defaults: { format: :json }
   resources :flashcards, only: [:index, :create], defaults: { format: :json }
 end
+
