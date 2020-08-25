97723c98ca24a4d9850b4f7f001325be1a7504b6
Santiago Rodriguez <santo1996.29@gmail.com>
2020-08-25 15:52:00 -0400

    fix: domain name in smtp configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,7 +108,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'spacer-repetition-api.herokuapp.com',
+    :domain         => 'spacer-repetition-api.heroku.com',
     :authentication => :plain,
   }
 
