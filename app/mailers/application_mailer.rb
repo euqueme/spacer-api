46c75b9128c54bb9e8e18a7dd73001b7aae867e7
Santiago Rodriguez <santo1996.29@gmail.com>
2020-08-25 17:51:02 -0400

    fix: mailgun configurations

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'spaced-repetition.no-reply@mailgun.net'
+  default from: 'spaced-repetition.no-reply@sandbox84d371bcf5924ad1bc3f53bd72fee228.com'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,7 +108,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'spacer-repetition-api.heroku.com',
+    :domain         => ENV['MAILGUN_DOMAIN'],
     :authentication => :plain,
   }
 
